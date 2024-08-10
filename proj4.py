3bd01140a949373a6cf0304da7ebb1c6a70eff81
Bhuvan1611 <bhuvaneshwaran1611@gmail.com>
2024-08-10 19:45:05 +0530

    Update proj4.py

--- proj4.py
+++ proj4.py
@@ -7,17 +7,5 @@ response = requests.get(url)
 print("JSON Response:", response.json())
 
 
-5.import requests
-
-url = 'http://api.open-notify.org/iss-now.json'
-
-response = requests.get(url)
-
-data = response.json()
-
-print("Latitude:", data['iss_position']['latitude'])
-print("Longitude:", data['iss_position']['longitude'])
-print("Timestamp:", data['timestamp'])
-
 
 
