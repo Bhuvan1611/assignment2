89c6b422d59bcac97c0af8f1aa85a1d3dbccbb95
Bhuvan1611 <bhuvaneshwaran1611@gmail.com>
2024-08-10 19:43:43 +0530

    Create proj4.py

---
+++ proj4.py
@@ -0,0 +1,23 @@
+import requests
+
+url = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(url)
+
+print("JSON Response:", response.json())
+
+
+5.import requests
+
+url = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(url)
+
+data = response.json()
+
+print("Latitude:", data['iss_position']['latitude'])
+print("Longitude:", data['iss_position']['longitude'])
+print("Timestamp:", data['timestamp'])
+
+
+
